6e8c8e75904b21be01b5f032a14d66df4f9f09b3
Ernesto Alba <ernestoalba@ciencias.unam.mx>
2015-03-23 11:51:07 -0600

    modulo intervalos con aritmética

--- Intervalos.jl
+++ Intervalos.jl
@@ -1,15 +1,47 @@
 module Intervalos
-	export Interval
-	typealias prec BigFloat
-	type Interval
-	lo
-	hi
-		function Interval(a, b)
-		set_rounding(prec, RoundDown)
-		lo = BigFloat("$a")
-		set_rounding(prec, RoundUp)
-		hi = BigFloat("$b")
-		new(lo, hi)
-		end
-	end
+export Interval
+
+typealias prec BigFloat
+
+type Interval
+    lo::Real
+    hi::Real   
+        function Interval(a, b)
+        set_rounding(prec, RoundDown)
+        lo = BigFloat("$a")
+        set_rounding(prec,RoundUp)
+        hi = BigFloat("$b")
+        if a>b 
+        error("el intervalo está al revés")
+        end
+        new(lo, hi)
+        end
 end
+
+
+function Interval(f)  #Para definir los escalares ya no nos importa que 
+    Interval(f,f)              #se definan intervalos de un sólo punto
+end
+
+function +(v::Interval, w::Interval)    
+    Interval(v.lo+w.lo,v.hi+w.hi)
+end
+
+function -(v::Interval, w::Interval) #la resta invierte el orden en los intervalos
+    Interval(v.lo-w.hi,v.hi-w.lo)   
+end
+
+function *(v::Interval, w::Interval) 
+    Interval(min(v.lo*w.lo,v.lo*w.hi,v.hi*w.lo,v.hi*w.hi),max(v.lo*w.lo,v.lo*w.hi,v.hi*w.lo,v.hi*w.hi))    
+
+end
+    
+function /(v::Interval, w::Interval) 
+    Interval(min(v.lo/w.lo,v.lo/w.hi,v.hi/w.lo,v.hi/w.hi),max(v.lo/w.lo,v.lo/w.hi,v.hi/w.lo,v.hi/w.hi))
+end 
+
+
+
+
+end
+
