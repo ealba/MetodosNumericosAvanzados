2bd5baef1e9b8727afbcb2d96310e86777182063
Ernesto Alba <ernestoalba@ciencias.unam.mx>
2015-04-11 19:43:31 -0500

    cambios extras

--- Intervalos.jl
+++ Intervalos.jl
@@ -1,7 +1,11 @@
 module Intervalos
 import Base.in
 import Base.==
-export Interval, in, == , redonUP , redonDOWN
+import Base.log
+import Base.exp
+import Base.^
+
+export Interval, in, == , redonUP , redonDOWN, log , exp, ^, monotona
 #NO ME PREOCUPÉ POR DIVIDIR ENTRE CEROS
 
 
@@ -13,9 +17,9 @@ type Interval
     
         function Interval(a, b)
         a, b = a > b ? (b,a) : (a,b)      #invierte el intervalo si está al revés  
-        #set_rounding(prec, RoundDown)
+        set_rounding(prec, RoundDown)
         lo = BigFloat("$a")
-        #set_rounding(prec,RoundUp)
+        set_rounding(prec,RoundUp)
         hi = BigFloat("$b")
         new(lo, hi)
         end
@@ -30,12 +34,27 @@ function redonUP(f::Function,x,y)
         f(BigFloat(x),BigFloat(y))
     end
 end
+function redonUP(f::Function,x)
+    with_rounding(BigFloat,RoundUp) do 
+        f(BigFloat(x))
+    end
+end
+
+
 
 function redonDOWN(f::Function,x,y)
     with_rounding(BigFloat,RoundDown) do 
         f(BigFloat(x),BigFloat(y))
     end
 end
+
+
+function redonDOWN(f::Function,x)
+    with_rounding(BigFloat,RoundDown) do 
+        f(BigFloat(x))
+    end
+end
+
 
 function in(x::Real,l::Interval)
 	#manera fácil:
@@ -60,10 +79,8 @@ end
 
 function *(v::Interval, w::Interval) 
     Interval(min(redonDOWN(*,v.lo,w.lo),redonDOWN(*,v.lo,w.hi),redonDOWN(*,v.hi,w.lo),redonDOWN(*,v.hi,w.hi)),max(redonUP(*,v.lo,w.lo),redonUP(*,v.lo,w.hi),redonUP(*,v.hi,w.lo),redonUP(*,v.hi,w.hi)))    
-
 end
     
-
 function /(v::Interval, w::Interval)
     if (0 in w)
         return error("El intervalo denominador contiene al cero")
@@ -73,11 +90,50 @@ function /(v::Interval, w::Interval)
 end 
 
 
+function monotona(f::Function,v::Interval) #para cualquier función monótona, si es decreciente, se invierte el intervalo
+        Interval(redonDOWN(f,v.lo),redonUP(f,v.hi))
+end 
+
+
+function log(v::Interval)   # logaritmo del valor absoluto de una función
+    if (v.lo < 0 || v.hi < 0)
+        return throw(DomainError()) #("el logaritmo no admite números negativos")
+        elseif (v.lo == 0)
+            return Interval(-Inf,-Inf) 
+        else
+            return monotona(log,v)
+        end
+end 
+
+
+function exp(v::Interval)
+    monotona(exp,v)
+end 
+
+function ^(v::Interval, w::Integer)
+    
+    if (v.hi > 0 && v.lo > 0)
+        return Interval(v.lo^w,v.hi^w)
+        elseif (v.hi < 0 && v.lo < 0)
+        return Interval(v.hi^w,v.lo^w)
+        else        
+        return Interval(0,max(v.lo^2,v.hi^2))
+    end
+end 
+
+
+function ^(v::Interval, q::Real) #Definido sólo para intervalos positivos
+    exp(Interval(q)*log(v))
+end 
+
+
+
+#en sí, cualquier función monótona la defines como monotona(función, intervalo)
+
+
 
 function ==(a::Interval, b::Interval)
 	(a.lo == b.lo && a.hi ==b.hi) && return true 
 	return false
 end
-
-
 end
--- IntervalosTest.jl
+++ IntervalosTest.jl
@@ -8,19 +8,32 @@ using Intervalos
 	B = Interval(1,2.0);
 	C = Interval(5.0,6.0);
 	D = Interval(-1,1); 
-  E = Interval(1.10,1.101);
-  F = Interval(0.1 , 0.1);  
-  x = 0.1; 
-  y = 1.1;  
+    E = Interval(1.10,1.101);
+    F = Interval(0.1 , 0.1);  
+    G = Interval(1,4)^(1/2);
+    H = Interval(1,2);
+    I=1000000.1
+    J=Interval(I)
+    K=(J^2)
+    x = 0.1; 
+    y = 1.1;  
+    
     @fact A => B
     @fact A+B => Interval(2)*A
     @fact X1-Y1 => Interval(1,8)
     @fact A/B => Interval(0.5,2)
     @fact A*B => Interval(1,4)
     @fact A*D => Interval(-2,2)
+    @fact H.lo in G  &&  H.hi in G=> true
+   
+    
+    @fact_throws DomainError Interval(-1,2)^(1/4) "No está bien definida la potencia para negativos"
+    
     @fact 0 in D=> true
     @fact y in E=> true
     @fact x in F=> true
-end
+    @fact 1000000200000.01 in K => true
+   
 
+end
 
